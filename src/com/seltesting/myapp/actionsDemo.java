bff830582f8d72f8c58429f22424d33025800cf2
sachin <montisimmi1617@gmail.com>
2025-05-28 11:52:15 +0530

    commit from Asian timeZone

--- src/com/seltesting/myapp/actionsDemo.java
+++ src/com/seltesting/myapp/actionsDemo.java
@@ -20,7 +20,9 @@ public class actionsDemo {
 		WebElement move= driver.findElement(By.cssSelector("div[id='nav-link-accountList']"));
 		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().perform();
 		a.moveToElement(move).contextClick().build().perform();
-		
+		System.out.println("Git file updated code 1");
+		System.out.println("Git file updated code 2");
+		System.out.println("Git file updated code 3");
 	}
 
 }
